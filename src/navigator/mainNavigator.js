1923f7f5d63dcdbbfa5583391bfbb62c7076b3f7
evan-carlsen <60202545+evan-carlsen@users.noreply.github.com>
2020-12-03 12:26:43 -0800

    Update mainNavigator.js

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,7 +5,9 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import PrivacyPolicyScreen1181974Navigator from '../features/PrivacyPolicyScreen1181974/navigator';
 import BlankScreen1181975Navigator from '../features/BlankScreen1181975/navigator';
+
 
 /**
  * new navigators can be imported here
@@ -14,6 +16,7 @@ import BlankScreen1181975Navigator from '../features/BlankScreen1181975/navigato
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+PrivacyPolicyScreen1181974: { screen: PrivacyPolicyScreen1181974Navigator },
 BlankScreen1181975: { screen: BlankScreen1181975Navigator },
 
     /** new navigators can be added here */
