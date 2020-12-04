88856a9f38284c7dec923ba23878469dca10fe78
evan-carlsen <60202545+evan-carlsen@users.noreply.github.com>
2020-12-03 18:35:16 -0800

    Update installed_blueprints.js

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,8 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
-  
-{ name: 'PrivacyPolicyScreen1181974', human_name: 'Privacy Policy', access_route: 'PrivacyPolicyScreen1181974'},
-{ name: 'BlankScreen1181975', human_name: 'Home Screen', access_route: 'BlankScreen1181975'},
+
 
   
   // you can add more installed blueprints here
